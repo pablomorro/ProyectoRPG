388f67a92cfa3c920f8b10ab29c11209e1d307f1
pablomorro <al361654@uji.es>
2018-12-26 00:26:57 +0100

    6

---
+++ RPG/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMaster : MonoBehaviour
+{
+    public static GameMaster sharedInstance;
+
+    //public bool showOptions = false;
+    //public float musicVolume = 0f, sfxVolume = 0f;
+
+    public GameObject inventoryPanel;
+
+    public InventorySistem inventory;
+    
+    private void OnEnable()
+    {
+        if (GameObject.FindGameObjectWithTag("InventoryUI")) {
+            inventoryPanel = GameObject.FindGameObjectWithTag("InventoryUI");
+            inventoryPanel.SetActive(false);
+        }
+    }
+
+    private void Awake()
+    {
+        if (sharedInstance == null)
+        {
+            sharedInstance = this;
+
+            inventory = new InventorySistem();
+
+            InventoryItem tempItem = new InventoryItem();
+
+            tempItem.Category = BaseItem.ItemCategory.Clothing;
+            tempItem.Name = "Capa mágica";
+            tempItem.Description = "La peor capa del juego";
+            tempItem.Strength = 0.5f;
+            tempItem.Weight = 0.1f;
+
+            inventory.AddItem(tempItem);
+        }
+        else if (sharedInstance != this) Destroy(this);
+
+        DontDestroyOnLoad(this);
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (currentScene.name != SceneName.mainMenu) //No estamos en el menu principal
+        if (Input.GetKeyDown(KeyCode.I)) {
+            inventoryPanel.SetActive(!inventoryPanel.activeInHierarchy);
+        }
+    }
+
+    public void RpgDestroy(GameObject item) {
+        Destroy(item);
+    }
+}
---
+++ RPG/Assets/Scripts/HUD/InventorySistem.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class InventorySistem 
+{
+
+    [SerializeField] private List<InventoryItem> weapons = new List<InventoryItem>();
+    [SerializeField] private List<InventoryItem> armour = new List<InventoryItem>();
+    [SerializeField] private List<InventoryItem> clothing = new List<InventoryItem>();
+    [SerializeField] private List<InventoryItem> health = new List<InventoryItem>();
+    [SerializeField] private List<InventoryItem> potion = new List<InventoryItem>();
+
+    private InventoryItem selectedWeapon;
+    private InventoryItem selectedArmour;
+
+    public InventoryItem SelectedWeapon {
+        get { return selectedWeapon; }
+        set { SelectedWeapon = value; }
+    }
+
+    public InventoryItem SelectedArmour {
+        get { return selectedArmour; }
+        set { selectedArmour = value; }
+    }
+
+    public InventorySistem() {
+        ClearInventory();
+    }
+
+    public void ClearInventory() {
+        weapons.Clear();
+        armour.Clear();
+        clothing.Clear();
+        health.Clear();
+        potion.Clear();
+    }
+
+    public void AddItem(InventoryItem item) {
+        switch (item.Category) {
+            case BaseItem.ItemCategory.Weapon:
+                weapons.Add(item);
+                break;
+
+            case BaseItem.ItemCategory.Armour:
+                armour.Add(item);
+                break;
+
+            case BaseItem.ItemCategory.Clothing:
+                clothing.Add(item);
+                break;
+
+            case BaseItem.ItemCategory.Health:
+                health.Add(item);
+                break;
+
+            case BaseItem.ItemCategory.Potion:
+                potion.Add(item);
+                break;
+        }
+    }
+
+    public void DeleteItem(InventoryItem item) {
+        switch (item.Category) {
+            case BaseItem.ItemCategory.Weapon:
+                weapons.Remove(item);
+                break;
+
+            case BaseItem.ItemCategory.Armour:
+                armour.Remove(item);
+                break;
+
+            case BaseItem.ItemCategory.Clothing:
+                clothing.Remove(item);
+                break;
+
+            case BaseItem.ItemCategory.Health:
+                health.Remove(item);
+                break;
+
+            case BaseItem.ItemCategory.Potion:
+                potion.Remove(item);
+                break;
+        }
+    }
+
+
+
+}
